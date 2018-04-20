4503bf383a14bea1778c58edb65e09605009ce1b
smcelfresh <sandyd.mcelfresh@gmail.com>
2018-04-20 16:35:24 -0500

    edited TemplateCard to Template

--- client/src/components/Template.js
+++ client/src/components/Template.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 
-const TemplateCard = (props) => {
+const Template = (props) => {
   
 render(){
 	return (
@@ -27,4 +27,4 @@ render(){
 	}
 }
 
-export default TemplateCard;
+export default Template;
