867db3655a5fa2c8c99f36232c20a98ae00521a0
smcelfresh <sandyd.mcelfresh@gmail.com>
2018-04-20 16:29:58 -0500

    completed Template component code

--- client/src/components/Template.js
+++ client/src/components/Template.js
@@ -3,3 +3,28 @@
 //Should be like a card with a title, image, and two buttons.
 //First button should be labeled "Source"
 //Second button should be labeled "Demo"
+
+import React from 'react';
+
+const TemplateCard = (props) => {
+  
+render(){
+	return (
+		<div className="card-user-profile">
+		  <img className="card-user-profile-img" src="https://images.pexels.com/photos/5439/earth-space.jpg?h=350&auto=compress&cs=tinysrgb" alt="picture of space" />
+		  <div className="card-user-profile-content card-section">
+		    <p className="card-user-profile-name">Carbo Templates</p>
+		    <p className="card-user-profile-status">We do the heavy lifting</p>
+		    <p className="card-user-profile-info">The Yeti, once better known as the Abominable Snowman, is a mysterious bipedal creature said to live in the mountains of Asia. It sometimes leaves tracks in snow, but is also said to dwell below the Himalayan snow line.</p>
+		  </div>
+
+		  <div className="card-user-profile-actions">
+		    <a href="#" className="card-user-profile-button button hollow">Source</a>
+		    <a href="#" className="card-user-profile-button button hollow secondary">Demo</a>
+		  </div>
+		</div>
+		)
+	}
+}
+
+export default TemplateCard;
