8fd214af686d7c061e68c1aea2bea9f9c91096be
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-04-22 18:49:32 -0500

    Reformatted

--- client/src/pages/Team/Team.js
+++ client/src/pages/Team/Team.js
@@ -1,36 +1,58 @@
-//TODO: Update Team page
-//Add style
-
 import React from "react";
 import "./Team.css";
 
 const teamMembers = [
     {
-        id: 1,
         name: "Carl Liu",
         image: "https://via.placeholder.com/100x100",
-        description: "Project Lead",
-        hometown: "Orange County, CA"
+        role: "Project Lead",
+        description: "blah blah"
+    },
+    {
+        name: "Sandy McElfresh",
+        image: "https://via.placeholder.com/100x100",
+        role: "Database Manager",
+        description: "blah blah"
+    },
+    {
+        name: "Paul Prokosch",
+        image: "https://via.placeholder.com/100x100",
+        role: "Tester",
+        description: "blah blah"
+    },
+    {
+        name: "Derek Jacobson",
+        image: "https://via.placeholder.com/100x100",
+        role: "Developer",
+        description: "blah blah"
+    },
+    {
+        name: "Kevin Hinkel",
+        image: "https://via.placeholder.com/100x100",
+        role: "Graphic Artist",
+        description: "blah blah"
     }
 ];
 
 const Team = () => (
 
-    <div>
+    <div className="team-page grid-x">
+        <div className="team-header cell small-12 medium-12">
+            <h1>Meet the Carbos Team</h1>
+        </div>
         {
             teamMembers.map((member) => (
-                <div className="blogpost-footer-author-section">
-
-                    <div>
-                        <a href="#" className="blogpost-footer-author">
-                            <img className="avatar" src={member.image} alt="" />
-                            <div>
-                                <p className="author">{member.name}</p>
-                                <p className="bio">{member.description}</p>
-                            </div>
-                        </a>
+                <div className="testimonial-block-vertical cell small-12 medium-6">
+                    <div className="testimonial-block-vertical-quote">
+                        <p>{member.description}</p>
                     </div>
-
+                    <div className="testimonial-block-vertical-person">
+                        <img className="testimonial-block-vertical-avatar" src={member.image} alt="" />
+                        <div>
+                            <p className="testimonial-block-vertical-name">{member.name}</p>
+                            <p className="testimonial-block-vertical-info">{member.role}</p>
+                        </div>
+                    </div>
                 </div>
             ))
         }
