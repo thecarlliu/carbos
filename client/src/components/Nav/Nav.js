62e55575cac24cc35159167edc7786260ff430ef
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-04-22 17:40:03 -0500

    Animated Nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,25 +6,25 @@ import "./Nav.css"
 
 const Nav = () => (
 	<div>
-        <div className="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
-            <button className="menu-icon" type="button" data-toggle="main-menu"/>
-            <div className="title-bar-title">Menu</div>
-        </div>
-        <div className="top-bar" id="main-menu">
-            <div className="top-bar-left">
-                <ul className="dropdown menu" data-dropdown-menu>
-                    <li className="menu-text">Carbos</li>
-                </ul>
+        <nav className="top-bar topbar-responsive">
+            <div className="top-bar-title">
+    <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+      <button className="menu-icon" type="button" data-toggle="topbar-responsive"/>
+    </span>
+                <a className="topbar-responsive-logo" href="/"><strong>Carbos</strong></a>
             </div>
-            <div className="top-bar-right">
-                <ul className="menu vertical medium-horizontal expanded" data-responsive-menu="drilldown medium-dropdown">
-                    <li><a href="/docs">Docs</a></li>
-                    <li><a href="/templates">Templates</a></li>
-                    <li><a href="/forum">Forum</a></li>
-                    <li><a href="/team">Team</a></li>
-                </ul>
+            <div id="topbar-responsive" className="topbar-responsive-links">
+                <div className="top-bar-right">
+                    <ul className="menu simple vertical medium-horizontal">
+                        <li><a href="/docs">Docs</a></li>
+                        <li><a href="/templates">Templates</a></li>
+                        <li><a href="/forum">Forum</a></li>
+                        <li><a href="/team">Team</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
+
     </div>
 );
 
