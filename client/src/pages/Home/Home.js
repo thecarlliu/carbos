d3fed6021fd5b3ed0aaab1e6a1ef827a5bf524a0
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-04-22 20:02:51 -0500

    Created Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,23 +1,90 @@
-//TODO: Create Home Page
-
-//From top to bottom:
-//A full-wide and animated section that shows our slogan, and welcomes users to the site.
-//Below, we want three cards to represent the Docs, Templates, and Forum sections.
-//Each section should link to the appropriate route.
-
-//This page is the best place to express our creativity. Themes, patterns, characters, designs, whatever -- The content on this page has to "hook" our customer.
 import React from "react";
 import "./Home.css";
 
+const features = [
+    {
+        name: "Aesthetic",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        icon: "fi fi-puzzle"
+    },
+    {
+        name: "Aesthetic",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        icon: "fi fi-puzzle"
+    },
+    {
+        name: "Aesthetic",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        icon: "fi fi-puzzle"
+    }
+];
+
+const techStack = [
+    {
+        name:"MongoDB",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        image:"https://via.placeholder.com/200x200",
+        link:"https://via.placeholder.com/200x200"
+    },
+    {
+        name:"Express",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        image:"https://via.placeholder.com/200x200",
+        link:"https://via.placeholder.com/200x200"
+    },
+    {
+        name:"React",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        image:"https://via.placeholder.com/200x200",
+        link:"https://via.placeholder.com/200x200"
+    },
+    {
+        name:"Node",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.",
+        image:"https://via.placeholder.com/200x200",
+        link:"https://via.placeholder.com/200x200"
+    }
+];
+
 const Home = () => (
-    <div>
-        <div className="marketing-site-hero">
+    <div className="grid-x">
+{/*hero*/}
+        <div className="marketing-site-hero cell small-12 medium-12">
             <div className="marketing-site-hero-content">
                 <h1>Welcome to Carbos</h1>
                 <p className="subheader">Striving for aesthetic and immersive interfaces.</p>
-                <a href="/templates" className="round button">learn more</a>
+                <a href="/#" className="round button">learn more</a>
             </div>
         </div>
+{/*features*/}
+        <section className="marketing-site-three-up grid-x small-12 medium-12">
+                <h2 className="marketing-site-three-up-headline cell small-12 medium-12">Let Carbos do the heavy lifting for you</h2>
+                {
+                    features.map((feature) => (
+                        <div className="feature-section cell small-12 medium-4">
+                            <i className={feature.icon} aria-hidden="true"/>
+                            <h4 className="marketing-site-three-up-title">{feature.name}</h4>
+                            <p className="marketing-site-three-up-desc">{feature.description}</p>
+                        </div>
+                    ))
+                }
+        </section>
+{/*techstack*/}
+        <div className="marketing-site-features grid-x small-12 medium-12">
+            <h2 className="marketing-site-features-headline cell small-12 medium-12">Our Technology Stack</h2>
+            <p className="marketing-site-features-subheadline subheader cell small-12 medium-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit quaerat eos words and phrases.</p>
+            {
+                techStack.map((tech) => (
+                    <div className="tech-section cell small-12 medium-6">
+                        <img src={tech.image} alt={tech.name} aria-hidden="true"/>
+                        <a href={tech.link}><h4 className="marketing-site-features-title">{tech.name}</h4></a>
+                        <p className="marketing-site-features-desc">{tech.description}</p>
+
+                    </div>
+                ))
+            }
+        </div>
+
     </div>
 );
 
